fd32cff8df6a74608ccf7be9dac55b4e7a9f43ed
abhisingh3960 <94777478+abhisingh3960@users.noreply.github.com>
2024-10-24 14:47:23 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,31 @@
 //your JS code here. If required.
+let inputAge = document.getElementById('age');
+let inputName = document.getElementById('name');
+let inputBtn = document.getElementById('btn');
+inputBtn.addEventListener('click' , (e)=>{
+	e.preventDefault();
+	let age = inputAge.value;
+	let name = inputName.value;
+	if(age ==="" && name===""){
+		return alert("inputs cannot be empty.");
+	}
+
+	
+	let p = new Promise((res , rej)=>{
+		setTimeout(()=>{
+		if(age>18){
+			res(`Welcome, ${name}. You can vote.`)
+		}
+			else{
+				rej(`Oh sorry  ${name}. You aren't old enough`)
+			}
+	},4000)
+	})
+
+	p.then((res)=>{
+		console.log(res);
+	})
+	.catch((err)=>{
+		console.log(err);
+	})
+})
